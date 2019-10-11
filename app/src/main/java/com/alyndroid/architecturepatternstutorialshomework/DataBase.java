e8b3ba350357a5faf74d7e53eed3486857a2209d
alinasser96 <ali96nasser@gmail.com>
2019-10-11 15:17:12 +0200

    First commit

---
+++ app/src/main/java/com/alyndroid/architecturepatternstutorialshomework/DataBase.java
@@ -0,0 +1,7 @@
+package com.alyndroid.architecturepatternstutorialshomework;
+
+public class DataBase {
+    public NumberModel getNumbers(){
+        return new NumberModel(4, 2);
+    }
+}
---
+++ app/src/main/java/com/alyndroid/architecturepatternstutorialshomework/NumberModel.java
@@ -0,0 +1,17 @@
+package com.alyndroid.architecturepatternstutorialshomework;
+
+public class NumberModel {
+    private int firstNum, secondNum;
+
+    public NumberModel(int firstNum, int secondNum) {
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+    }
+
+    public int getFirstNum() {
+        return firstNum;
+    }
+    public int getSecondNum() {
+        return secondNum;
+    }
+}
